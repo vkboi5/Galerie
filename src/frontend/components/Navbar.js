81e7db1d0a08f683f771d181f7eaf9d568e1c83b
Sarang Kadukar <sarangkadukar9@gmail.com>
2024-07-03 04:31:53 +0530

    navbar

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -17,7 +17,7 @@ const Navigation = ({ web3Handler, account }) => {
     return (
         <Navbar expand="lg" bg="secondary" variant="dark" className="custom-navbar">
             <Navbar.Brand as={Link} to="/">
-                <img src={logo} width="100" height="40" className="logo-img" alt="Logo" />
+                <img src={logo} className="logo-img" alt="Logo" />
             </Navbar.Brand>
             <div className="connect-wallet-wrapper">
                 {account && (
